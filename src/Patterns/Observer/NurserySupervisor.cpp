823c7c79649c5a7325ca78b5c3ba2c7a873cf51e
DandreNel7 <u24600726@tuks.co.za>
2025-10-23 21:36:35 +0200

    removed nursery from branch

--- src/Patterns/Observer/NurserySupervisor.cpp
+++ src/Patterns/Observer/NurserySupervisor.cpp
@@ -1,19 +1,7 @@
 #include "../../../include/Patterns/Observer/NurserySupervisor.h"
-#include "../../../include/Core/Nursery.h"
-#include "../../../include/Patterns/Command/WaterPlantCommand.h"
-#include "../../../include/Patterns/Command/WaterPlantCommand.h"
-#include "../../../include/Components/Plant.h"
 
 NurserySupervisor::NurserySupervisor(const std::shared_ptr<Nursery>& nursery) : nursery(nursery) {}
 
 void NurserySupervisor::update(const std::shared_ptr<Subject>& subject) {
-    auto plant = std::dynamic_pointer_cast<Plant>(subject);
-    if (!plant) return; 
-
-    auto nurseryPtr = nursery.lock();
-    if (!nurseryPtr) return;  
-
-    if (plant->getWaterLevel() < plant->getWaterConsumption()) {
-        nurseryPtr->addRequest(std::make_unique<WaterPlantCommand>(plant));
-    }
+    (void)subject;  // stub: real behavior implemented later
 }
