a4d3a920ceee693644a466c7c4a957cdd9790680
stephan-kritzinger <u24581705@tuks.co.za>
2025-10-25 10:07:34 +0200

    Added mature logic

--- src/Patterns/State/Mature.cpp
+++ src/Patterns/State/Mature.cpp
@@ -2,10 +2,18 @@
 
 Mature::Mature() = default;
 
-void Mature::handleStateChange(Plant* plant) { (void)plant; }
+void Mature::handleStateChange(Plant* plant) {
+    if(plant->getHealth() <= 0){
+        plant->setState(std::make_unique<Withering>(std::make_unique<Mature>()));
+    }
+}
 void Mature::performDailyActivity(Plant* plant) {
     plant->setWaterLevel(plant->getWaterLevel() - plant->getWaterConsumption());
-    
-
+    //Should mature plants still be cared for.
+    if(plant->getWaterLevel() <= 0){
+        plant->setHealth(plant->getHealth() - 40); //Amount can be adjusted, but it takes 2 days of fertilising to recover an uncared plant fully
+    }
+    //Price functionality?
+    handleStateChange(plant);
 }
 std::unique_ptr<PlantState> Mature::clone() const { return std::make_unique<Mature>(); }
