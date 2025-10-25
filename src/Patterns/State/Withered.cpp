df7d0adb0d7c8451d19b790d8ebc086071de45e3
stephan-kritzinger <u24581705@tuks.co.za>
2025-10-25 10:04:39 +0200

    Added withered logic, mostly just stubs

--- src/Patterns/State/Withered.cpp
+++ src/Patterns/State/Withered.cpp
@@ -2,6 +2,8 @@
 
 Withered::Withered() = default;
 
+//Plant is irrecoverable, stubbed
 void Withered::handleStateChange(Plant* plant) { (void)plant; }
+//Stubbed, unless price logic should be added, but it should probably just be 0
 void Withered::performDailyActivity(Plant* plant) { (void)plant; }
 std::unique_ptr<PlantState> Withered::clone() const { return std::make_unique<Withered>(); }
