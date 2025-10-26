0ae9de54e9207af73f861203a5fa21c98790b9c6
Sebastian-Pienaar <u24778232@tuks.co.za>
2025-10-26 19:43:58 +0200

    Lavender, Orchid, Petunia factory header and implementation files

---
+++ src/Patterns/Factory/OrchidFactory.cpp
@@ -0,0 +1,7 @@
+#include "../../../include/Patterns/Factory/OrchidFactory.h"
+
+#include "../../../include/Components/Orchid.h"
+
+OrchidFactory::OrchidFactory() = default;
+
+std::shared_ptr<Plant> OrchidFactory::createPlant() { return std::make_shared<Orchid>(); }
