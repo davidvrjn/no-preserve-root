6196e92447ba70504cc50a7ba33d024da2cc2b51
ZiphoZ <innocentiamaduna26@gmail.com>
2025-10-27 11:16:38 +0200

    Implement NurserySupervisor update() method to create WaterPlantCommand

--- src/Patterns/Observer/NurserySupervisor.cpp
+++ src/Patterns/Observer/NurserySupervisor.cpp
@@ -1,7 +1,24 @@
 #include "../../../include/Patterns/Observer/NurserySupervisor.h"
+#include "../../../include/Patterns/Command/WaterPlantCommand.h"
+#include "../../../include/Core/Nursery.h"
+#include "../../../include/Components/Plant.h"
 
 NurserySupervisor::NurserySupervisor(const std::shared_ptr<Nursery>& nursery) : nursery(nursery) {}
 
 void NurserySupervisor::update(const std::shared_ptr<Subject>& subject) {
-    (void)subject;  // stub: real behavior implemented later
+    // Cast to Plant to access plant-specific method
+    auto plant = std::dynamic_pointer_cast<Plant>(subject);
+    if(!plant) 
+        return;
+
+    // Lock the weak_pointer(nursery) to get a shared_pointer
+    auto nurseryPtr = nursery.lock();
+    if(!nurseryPtr) 
+        return;
+
+    // call the watercommand method
+    if(plant->getWaterLevel() < 50) {
+        auto cmd = std::make_unique<WaterPlantCommand>(plant);
+        nurseryPtr->addRequest(std::move(cmd));
+    }
 }
