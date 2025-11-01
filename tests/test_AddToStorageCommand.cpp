297e3947a7bf0c18b9d2885d51b436f000c1a37c
u24603199 <u24603199@tuks.co.za>
2025-11-01 21:25:29 +0200

    noo. Nope

--- tests/test_AddToStorageCommand.cpp
+++ tests/test_AddToStorageCommand.cpp
@@ -39,7 +39,7 @@ class DummyPlant : public Plant {
 // -----------------------------------------------------------------------------
 // Dummy Group
 // -----------------------------------------------------------------------------
-class DummyGroup : public Group, public std::enable_shared_from_this<DummyGroup> {
+class DummyGroup : public Group {
     std::vector<std::shared_ptr<InventoryComponent>> memberList;
 
    public:
@@ -52,7 +52,7 @@ class DummyGroup : public Group, public std::enable_shared_from_this<DummyGroup>
         }
 
         memberList.push_back(c);
-        c->setOwner(shared_from_this());
+        c->setOwner(std::static_pointer_cast<Group>(shared_from_this()));
     }
 
     void remove(const std::shared_ptr<InventoryComponent>& c) {
